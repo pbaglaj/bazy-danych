262591b3b7a4067cb02d7fb9d9cb96c026cb2639
Pawel Baglaj <184997716+pbaglaj@users.noreply.github.com>
2026-03-02 00:30:05 +0100

    fix routes

--- superhero-api/routes/heroRoutes.js
+++ superhero-api/routes/heroRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-const ctrl    = require('../controllers/userController');
+const ctrl    = require('../controllers/heroController');
 
 router.get('/',    ctrl.getAll);
 router.post('/', ctrl.create);
--- superhero-api/routes/incidentRoutes.js
+++ superhero-api/routes/incidentRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router  = express.Router();
-const ctrl    = require('../controllers/userController');
+const ctrl    = require('../controllers/incidentController');
 
 router.get('/',    ctrl.getAll);
 router.post('/',   ctrl.create);
-router.post('/:id/assign',   ctrl.assign);
-router.post('/:id/resolve',   ctrl.resolve);
+router.post('/:id/assign',   ctrl.assignHeroToIncident);
+router.post('/:id/resolve',   ctrl.closeIncident);
 
 module.exports = router;
