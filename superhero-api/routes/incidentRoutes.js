2ddfc2286bceff4990dbd089df14d278d11b516d
Pawel Baglaj <184997716+pbaglaj@users.noreply.github.com>
2026-03-02 01:22:44 +0100

    fix routes

--- superhero-api/routes/incidentRoutes.js
+++ superhero-api/routes/incidentRoutes.js
@@ -5,6 +5,6 @@ const ctrl    = require('../controllers/incidentController');
 router.get('/',    ctrl.getAll);
 router.post('/',   ctrl.create);
 router.post('/:id/assign',   ctrl.assignHeroToIncident);
-router.post('/:id/resolve',   ctrl.closeIncident);
+router.patch('/:id/resolve',   ctrl.closeIncident);
 
 module.exports = router;
